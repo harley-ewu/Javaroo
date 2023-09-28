c91d544a6b74c895eccf3a811f1e41707a808640
Oscar Ruelas <oruelas@ewu.edu>
2023-09-27 20:57:44 -0700

    hello world

---
+++ Interface/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+        }
+    }
