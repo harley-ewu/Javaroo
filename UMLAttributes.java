09138baa81d9c16fd06678a25c1e0d2af5d7b2af
jsanders25 <114096326+jsanders25@users.noreply.github.com>
2023-10-07 01:59:08 +0000

    Added a setter for name and comments

--- UMLAttributes.java
+++ UMLAttributes.java
@@ -1,18 +1,26 @@
 public class UMLAttributes
 {
+    // Unique identifier
     private String name;
 
+    // Constructor
     public UMLAttributes(String name)
     {
         this.name = name;
     }
 
+    // Getters and Setters
     public String getName()
     {
         return this.name;
     }
 
+    public void setName(String newName)
+    {
+        this.name = newName;
+    }
 
+    // Prints out the name of the Attribute
     @Override
     public String toString()
     {
