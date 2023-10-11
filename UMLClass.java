3fedbda61e841aced66d1729e35d2bebe98cecee
Roberto Garcia <129777137+robertogarcia11@users.noreply.github.com>
2023-10-11 19:36:02 +0000

    added methods for review

--- UMLClass.java
+++ UMLClass.java
@@ -4,45 +4,62 @@ public class Class {
 
     private String name;
     private ArrayList<UMLAttributes> attributes;
-    private ArrayList<UMLRelationships> relationships;
 
     public Class(String name) {
         this.name = name;
-        this.attributes = new ArrayList<UMLAttributes>();
-        this.relationships = new ArrayList<UMLRelationships>(); 
-
+        this.attributes = new ArrayList<>();
     }
 
-    public String getName() {
-        return name;
-
-    }
-
-    //addName method should fail if name is invalid or duplicate
-    public class addName(String name) {
-        if (!isNameValid(name)) {
-            return false;
+    public void add(String name) {
+        if (!isNameValid(name) || classList.contains(name)) {
+            System.out.println("Invalid or duplicated name.");
+        } else {
+            classList.add(name);
+            System.out.println("Name added: " + name);
+        }
         
+    }
+
+    // dont need rename method
+    /*public void rename(String oldName, String newName) {
+        if (!isNameValid(newName) || classList.contains(newName)) {
+            System.out.println("Invalid or duplicated name.");
+        } 
+        else if (!classList.contains(oldName)) {
+            System.out.println("Name not found.");
+        } 
+        else {
+            classList.set(classList.indexOf(oldName), newName);
+            System.out.println("Name renamed from " + oldName + " to " + newName);
         }
 
+    }*/
+
+    // rename method
+    public String getName() {
+        return this.name;
+
+    }
+
+    public void setName(String name) {
         this.name = name;
 
     }
 
-    //helper method to make sure that name is not invalid(empty or null)
-    private boolean isNameValid(String name) {
-        if (name == null || name.equals("")) {
-            return false;
-
+    public void delete(String name) {
+        if (!classList.contains(name)) {
+            System.out.println("Name not found.");
+        } else {
+            classList.remove(name);
+            System.out.println("Name deleted: " + name);
         }
-        return true;
 
     }
-    
 
-    public String toString() {
-        return name + " [Class]";
-
+    // helper method to check if name is valid
+    private boolean isNameValid(String name) {
+        return name != null && !name.isEmpty();
+        
     }
 
 }
