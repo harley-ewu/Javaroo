4d68ba9390eb1d9fca6df693ae129ae1ed78c71f
jsanders25 <114096326+jsanders25@users.noreply.github.com>
2023-10-11 18:36:41 +0000

    Completed addAttribute method

--- UMLAttributes.java
+++ UMLAttributes.java
@@ -20,6 +20,13 @@ public class UMLAttributes
         this.name = newName;
     }
 
+    public static void addAttribute(Class c, String name)
+    {
+        UMLAttributes attr = new UMLAttributes(name);
+        c.getAttributes().add(attr);
+        System.out.println("Attribute " + name + " added to class " + c.getName() + " successfully.");
+    }
+
     // Prints out the name of the Attribute
     @Override
     public String toString()
