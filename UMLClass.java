9e4d522afe54baf9d9faa7a35961ed017113c06c
Roberto Garcia <129777137+robertogarcia11@users.noreply.github.com>
2023-10-11 21:18:49 +0000

    added toString and changed methods for Hash

--- UMLClass.java
+++ UMLClass.java
@@ -5,37 +5,53 @@ public class Class {
     private String name;
     private ArrayList<UMLAttributes> attributes;
 
+    // constructor
     public Class(String name) {
         this.name = name;
         this.attributes = new ArrayList<>();
     }
 
-    // rename method
+    // getters method that will work as a rename method
     public String getName() {
         return this.name;
 
     }
-
+    // setters 
     public void setName(String name) {
         this.name = name;
 
     }
 
      public static void addClass(String name, UMLDiagram diagram) {
-        diagram.getClasses().add(new Class(name));
+        //diagram.getClasses().add(new Class(name));
+        diagram.getClasses().put(name, new Class(name));
         System.out.println("Class added: " + name);
         
     }
 
     public void deleteClass(UMLClass c, UMLDiagram diagram) {
+        // statement to check if class exists
         if(c == null) {
             System.out.println("Class not found");
             return;
         }
-        
-        diagram.getClasses().remove(c);
+        // if class exists, classes will be fetched and removed
+        diagram.getClasses().remove(c.getName());
         System.out.println("Class deleted: " + c.getName());
 
     }
+
+    // toString method
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Class: ").append(this.name).append("\n");
+        sb.append("Attributes:\n");
+        for (String attribute : this.attributes) {
+            sb.append(" • ").append(attribute).append("\n");
+        }
+        return sb.toString();
+
+}
+
 
 }
