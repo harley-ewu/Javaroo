88ca840155fef42bb92153eff702c4c4b9d06fa6
Oscar Ruelas <oruelas@ewu.edu>
2023-10-23 11:40:26 -0700

    added some css code

--- Project1.2/UMLDiagramGUI/src/main/java/com/example/umldiagramgui/UMLDiagram.java
+++ Project1.2/UMLDiagramGUI/src/main/java/com/example/umldiagramgui/UMLDiagram.java
@@ -8,14 +8,20 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class UMLDiagram extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Main.fxml")));
             stage.setTitle("UML Diagram Builder");
-            stage.setScene(new Scene(root));
+            Scene scene = new Scene(root, 300, 200);
+            scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("CSS.css")).toExternalForm());
+            stage.setScene(scene);
+            stage.show();
+
+            //stage.setScene(new Scene(root, 300, 200));
             stage.show();
         } catch (Exception e){
             e.printStackTrace();
