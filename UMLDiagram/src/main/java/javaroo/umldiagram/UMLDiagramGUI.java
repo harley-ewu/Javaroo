5aaf69d9fca24c5cb3c73fbdac149ae7224716d9
Oscar Ruelas <oruelas@ewu.edu>
2023-10-25 00:11:23 -0700

    added an icon

--- UMLDiagram/src/main/java/javaroo/umldiagram/UMLDiagramGUI.java
+++ UMLDiagram/src/main/java/javaroo/umldiagram/UMLDiagramGUI.java
@@ -3,16 +3,21 @@ package javaroo.umldiagram;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class UMLDiagramGUI extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(UMLDiagramGUI.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        Image appIcon = new Image(Objects.requireNonNull(UMLDiagramGUI.class.getResourceAsStream("icon.png")));
+        stage.getIcons().add(appIcon);
+        stage.setTitle("UML Diagram Builder");
         stage.setScene(scene);
         stage.show();
     }
