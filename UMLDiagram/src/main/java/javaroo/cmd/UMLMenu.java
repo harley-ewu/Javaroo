587d0b9b0143e5b4569b7883e8856badc70f6d26
Roberto Garcia <129777137+robertogarcia11@users.noreply.github.com>
2023-11-01 22:48:29 +0000

    menu implementation

--- UMLDiagram/src/main/java/javaroo/cmd/UMLMenu.java
+++ UMLDiagram/src/main/java/javaroo/cmd/UMLMenu.java
@@ -23,7 +23,7 @@ public class UMLMenu {
         boolean exit = false;
 
         while (!exit) {
-            System.out.println("\n___ Welcome to the UML Diagram Menu! ___");
+            System.out.println("\n___ Welcome to the UML Diagram Menu!😘 ___");
             System.out.println("Type 'r' to return to the main menu at any time.");
             System.out.println(" ");
             System.out.println("1: Class Options");
@@ -76,9 +76,9 @@ public class UMLMenu {
         while (!back) {
             System.out.println("\n________Class Operations:________");
             System.out.println("1: Add Class");
-            System.out.println("2: Delete Class");
+            System.out.println("2: Remove Class");
             System.out.println("3: Rename Class");
-            System.out.println("4: Attributes Options");
+            System.out.println("4: Field and Method Options");
             System.out.println("5: Help");
             
             System.out.print("Enter your choice: ");
@@ -90,47 +90,86 @@ public class UMLMenu {
             
             switch (choice) {
                 case "1":
-                    // update so user can also add attributes and methods here
-                    diagram.listClasses();
+                    // here user can add a class, they can also add fields and methods to the class if they wish
                     System.out.print("\nEnter the class name to be added: ");
                     String addClassName = scanner.nextLine();
                     UMLClass.addClass(addClassName);
+                    System.out.println("\nClass added: " + addClassName);
+                    System.out.println("\nWould you also like to add fields and methods to the class? (y/n)");
+                    String addFieldsMethods = scanner.nextLine();
+                    if (addFieldsMethods.equals("y")) {
+                        System.out.println("\nHow many fields would you like to add?");
+                        int numFields = scanner.nextInt();
+                        scanner.nextLine();
+                        for (int i = 0; i < numFields; i++) {
+                            System.out.println("\nEnter the field name: ");
+                            String fieldName = scanner.nextLine();
+                            System.out.println("\nEnter the field type: ");
+                            String fieldType = scanner.nextLine();
+                            UMLClass.addField(addClassName, fieldName, fieldType);
+                            System.out.println("\nField added.");
+                        }
+                        System.out.println("\nHow many methods would you like to add?");
+                        int numMethods = scanner.nextInt();
+                        scanner.nextLine();
+                        for (int i = 0; i < numMethods; i++) {
+                            System.out.println("\nEnter the method name: ");
+                            String methodName = scanner.nextLine();
+                            System.out.println("\nEnter the method type: ");
+                            String methodType = scanner.nextLine();
+                            UMLClass.addMethod(addClassName, methodName, methodType);
+                            System.out.println("\nMethod added.");
+                        }
+                    }
+                    // list added class with its fields and methods
                     diagram.listClasses();
                     returnToMainMenu();
-                    break;
+
                 case "2":
                     diagram.listClasses();
-                    System.out.print("\nEnter the class name to be deleted: ");
-                    String deleteClassName = scanner.nextLine();
-                    UMLClass c = diagram.classExists(deleteClassName);
-                    UMLClass.deleteClass(c, diagram);
+                    System.out.print("\nEnter the class name to be removed: ");
+                    String removeClassName = scanner.nextLine();
+                    // print updated list of classes
                     diagram.listClasses();
                     returnToMainMenu();
-                    break;
+
                 case "3":
                     diagram.listClasses();
-                    System.out.print("\nEnter the class name to be renamed: ");
+                    System.out.print("\nEnter the class name you want to renamed: ");
                     String oldClassName = scanner.nextLine();
                     System.out.print("\nEnter the new class name: ");
                     String newClassName = scanner.nextLine();
-                    UMLClass.renameClass(oldClassName, newClassName);
-                    System.out.println("\nClass renamed to " + newClassName + ".");
+                    System.out.println("\nClass renamed from: " + oldClassName + " to " + newClassName);
+                    // print updated list of classes
                     diagram.listClasses();
                     returnToMainMenu();
-                    break;
+
                 case "4":
-                    diagram.listClasses();
-                    System.out.print("\nEnter the class name: ");
-                    String className = scanner.nextLine();
-                    diagram.listClassContents(className); 
-                    returnToMainMenu();
-                    break;
+                    // menu option for add field/method, remove field/method, rename field/method
+                    System.out.println("\n______Field and Method Options:______");
+                    System.out.println("1: Add Field");
+                    System.out.println("2: Remove Field");
+                    System.out.println("3: Rename Field");
+                    System.out.println("4: Add Method");
+                    System.out.println("5: Remove Method");
+                    System.out.println("6: Rename Method");
+                    System.out.println("7: Help");
+
+                    System.out.print("Enter your choice: ");
+                    String choice2 = scanner.nextLine();
+
+                    switch(choice2)
+
+
+                    
+            }
+
                 case "5":
                     System.out.println("\nHelp with class operations:");
                     System.out.println("[1: Add Class] To add a class, select option '1' and provide the class name.");
                     System.out.println("[2: Delete Class] To delete a class, select option '2' and provide the class name.");
                     System.out.println("[3: Rename Class] To rename a class, select option '3' and provide the old and new class names.");
-                    System.out.println("[4: Attributes Options] To access attributes options, select option '4'.");
+                    System.out.println("[4: Field and Method Options] To access field and methods options, select option '4'.");
                     break;
 
                 case "6":
