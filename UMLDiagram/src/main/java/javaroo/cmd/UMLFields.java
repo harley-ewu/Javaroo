814e37a37e3b9bf30e5ee734c949901aaae6ac6d
jsanders25 <114096326+jsanders25@users.noreply.github.com>
2023-11-04 23:57:29 +0000

    Added unit tests

--- UMLDiagram/src/main/java/javaroo/cmd/UMLFields.java
+++ UMLDiagram/src/main/java/javaroo/cmd/UMLFields.java
@@ -5,6 +5,12 @@ public class UMLFields {
     private String visibility;
 
     public UMLFields(String name, String type, String visibility) {
+        //check for valid visibility
+        if(!visibility.equals("public") && !visibility.equals("private"))
+        {
+            System.out.println("Invalid visibility");
+            return;
+        }
         this.name = name;
         this.type = type;
         this.visibility = visibility;
