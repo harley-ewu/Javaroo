2478681d70411e2c7382f855b587ce60bbb137f5
Oscar Ruelas <114115100+Oscar-Ruelas@users.noreply.github.com>
2023-11-04 21:31:13 -0700

    Update UMLRelationships.java

--- UMLDiagram/src/main/java/javaroo/cmd/UMLRelationships.java
+++ UMLDiagram/src/main/java/javaroo/cmd/UMLRelationships.java
@@ -5,6 +5,11 @@ public class UMLRelationships {
     private UMLClass dest;
     private UMLClass source;
     private String id;
+
+    private double startX; // Starting X-coordinate
+    private double startY; // Starting Y-coordinate
+    private double endX; // Ending X-coordinate
+    private double endY; // Ending Y-coordinate
     RelationshipType type;
 
     public enum RelationshipType {
@@ -23,6 +28,22 @@ public class UMLRelationships {
         this.type = type;
     }
 
+    public UMLRelationships(final UMLClass source, final UMLClass dest, final RelationshipType type, final double startX, final double startY, final double endX, final double endY) {
+        // Check each parameter is not null
+        if (source == null || dest == null || type == null) {
+            System.out.println("Sorry, but we could not find a valid source, destination, or type for this relationship");
+            return;
+        }
+        this.source = source;
+        this.dest = dest;
+        this.id = source.getName() + dest.getName();
+        this.type = type;
+        this.startX = 0; // Initialize to default values
+        this.startY = 0;
+        this.endX = 0;
+        this.endY = 0;
+    }
+
     public UMLClass getDest() {
         return dest;
     }
@@ -37,5 +58,37 @@ public class UMLRelationships {
 
     public RelationshipType getType() {
         return type;
+    }
+
+    public double getStartX() {
+        return startX;
+    }
+
+    public void setStartX(double startX) {
+        this.startX = startX;
+    }
+
+    public double getStartY() {
+        return startY;
+    }
+
+    public void setStartY(double startY) {
+        this.startY = startY;
+    }
+
+    public double getEndX() {
+        return endX;
+    }
+
+    public void setEndX(double endX) {
+        this.endX = endX;
+    }
+
+    public double getEndY() {
+        return endY;
+    }
+
+    public void setEndY(double endY) {
+        this.endY = endY;
     }
 }
