858b6bc34c89e725bcd644647e4bbc48b8de2af0
Curtis Melton <curtisjmelton@gmail.com>
2023-11-25 13:08:00 -0800

    c1

--- UMLDiagram/src/main/java/module-info.java
+++ UMLDiagram/src/main/java/module-info.java
@@ -1,10 +1,29 @@
-module javaroo.umldiagram {
+/* module javaroo.umldiagram {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
+    requires junit;
 
 
     opens javaroo.umldiagram to javafx.fxml;
     //exports javaroo.umldiagram;
     exports javaroo.umldiagram to javafx.graphics;
+}
+
+
+ */
+
+
+module javaroo.umldiagram {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires com.google.gson;
+    requires junit;
+
+    opens javaroo.umldiagram to javafx.fxml;
+    // Added exports and opens directives for javaroo.cmd
+    exports javaroo.cmd to junit, javafx.graphics;
+    opens javaroo.cmd to junit;
+
+    // Other exports if necessary
 }
